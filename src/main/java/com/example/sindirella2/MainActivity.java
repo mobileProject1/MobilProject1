7f2dceade59953f4b41087b1ff76aa6793476d4e
demirbasbusraa <48440609+demirbasbusraa@users.noreply.github.com>
2020-02-28 14:39:04 +0300

    Add files via upload

---
+++ src/main/java/com/example/sindirella2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sindirella2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondpage);
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
